33585601108b3fc62a81b6711c4a6bc99ce9c335
fernando monteiro <fernando@newaeonweb.com.br>
2016-01-14 14:19:17 -0200

    added profile function and template

--- app/templates/app/modules/layouts/page/pageCtrl.js
+++ app/templates/app/modules/layouts/page/pageCtrl.js
@@ -13,7 +13,7 @@
 		.module('<%= slugifiedAppName %>')
 		.controller('LayoutCtrl', Layout);
 
-	Layout.$inject = ['$mdSidenav', '$cookies', '$state'];
+	Layout.$inject = ['$mdSidenav', '$cookies', '$state', '$mdToast', '$mdDialog'];
 
 	/*
 	 * recommend
@@ -21,13 +21,63 @@
 	 * and bindable members up top.
 	 */
 
-	function Layout($mdSidenav, $cookies, $state) {
+	function Layout($mdSidenav, $cookies, $state, $mdToast, $mdDialog ) {
 		/*jshint validthis: true */
 		var vm = this;
 
 		vm.toggleSidenav = function (menuId) {
 			$mdSidenav(menuId).toggle();
 		};
+
+		vm.changePassword = function () {
+			$mdToast.show(
+				$mdToast.simple()
+					.content('Password clicked!')
+					.position('top right')
+					.hideDelay(2000)
+			);
+		};
+
+		vm.changeProfile = function (ev) {
+				$mdDialog.show({
+					controller: DialogController,
+					templateUrl: 'tabDialog.tmpl.html',
+					parent: angular.element(document.body),
+					targetEvent: ev,
+					clickOutsideToClose:true
+				})
+				.then(function(answer) {
+					$mdToast.show(
+						$mdToast.simple()
+							.content('You said the information was "' + answer + '".')
+							.position('top right')
+							.hideDelay(2000)
+					);
+
+				}, function() {
+					$mdToast.show(
+						$mdToast.simple()
+							.content('You cancelled the dialog.')
+							.position('top right')
+							.hideDelay(2000)
+					);
+				});
+
+				function DialogController($scope, $mdDialog) {
+					$scope.hide = function() {
+						$mdDialog.hide();
+					};
+
+					$scope.cancel = function() {
+						$mdDialog.cancel();
+					};
+
+					$scope.answer = function(answer) {
+						$mdDialog.hide(answer);
+					};
+				}
+		};
+
 
 		vm.logOut = function () {
 
