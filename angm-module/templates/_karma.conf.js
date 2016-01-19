699e8b83d040f5b565eb58ba257a2d65e61d17df
fernando monteiro <fernando@newaeonweb.com.br>
2016-01-19 13:55:41 -0200

    added karma spec plugin

--- angm-module/templates/_karma.conf.js
+++ angm-module/templates/_karma.conf.js
@@ -42,7 +42,16 @@ module.exports = function (config) {
 		// Test results reporter to use
 		// Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
 		//reporters: ['progress'],
-		reporters: ['progress'],
+		reporters: ['spec'],
+
+		plugins : [
+			'karma-jasmine',
+			'karma-coverage',
+			'karma-chrome-launcher',
+			'karma-firefox-launcher',
+			'karma-phantomjs-launcher',
+			'karma-spec-reporter'
+		],
 
 		// Web server port
 		port: 9876,
